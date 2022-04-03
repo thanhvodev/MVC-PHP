41386eb0aec05efe8db0b9d07c068d0330be347e
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-04-03 17:08:10 +0700

    Initialize develop

---
+++ app/views/inc/head.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta htttp-equiv="Cache-control" content="no-cache">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="<?= SITE_NAME ?>">
+    <meta name="description" content="<?= CARD_DESCRIPTION ?>">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?= URL_ROOT ?>">
+    <meta property="og:title" content="<?= SITE_NAME ?>">
+    <meta property="og:description" content="<?= CARD_DESCRIPTION ?>">
+    <meta property="og:image" content="<?= CARD_IMAGE ?>">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="<?= URL_ROOT ?>">
+    <meta property="twitter:title" content="<?= SITE_NAME ?>">
+    <meta property="twitter:description" content="<?= CARD_DESCRIPTION ?>">
+    <meta property="twitter:image" content="<?= CARD_IMAGE ?>">
+
+    <!-- TabName -->
+    <?php if(isset($data['headTitle'])): ?><title><?= $data['headTitle'] ?> - <?= SITE_NAME ?></title><?php else: ?><title><?= SITE_NAME ?></title><?php endif; ?>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="<?= URL_ROOT ?>/public/css/normalize.css">
+    <link rel="stylesheet" href="<?= URL_ROOT ?>/public/css/global.style.css">
+    <?php if(isset($data['cssFile'])): ?><link rel="stylesheet" href="<?= URL_ROOT ?>/public/css/<?= $data['cssFile'] ?>.style.css"><?php endif; ?>
+
+    <!-- Load FontAwsome and Jquery -->
+    <?php echo file_get_contents('https://headswift.com/cdn/0.1.0/?frameworks=headswift-stats,font-awesome,jquery&version=0.1.0,5.15.3,3.6.0'); ?>
+</head>
+<body>
+
