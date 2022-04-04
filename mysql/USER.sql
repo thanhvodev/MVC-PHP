1b5d81abdbd31a2d6211bb82f2c577b1ae0e893c
Ha Bac Ngo <81097399+hbngo21@users.noreply.github.com>
2022-04-04 15:41:17 +0700

    Add database.

---
+++ mysql/USER.sql
@@ -0,0 +1,43 @@
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Thiên', '1', 'Đức Quang Thiên', 'ducquangthien107@facebook.com', '089 579 0132', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', '300 COUNTY ROAD 62 GRANT CO 80448-5013 USA', '1');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Quyên', '1', 'Lục Bích Quyên', 'lucbichquyen420@naver.com', '0123456789', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', '45900 US HIGHWAY 285 GRANT CO 80448-5021 USA', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Huệ', '1', 'Xung Minh Huệ', 'xungminhhue624@microsoft.com', '0123456789', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', '50400 US HIGHWAY 285 GRANT CO 80448-5029 USA', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Chiểu', '1', 'Khâu Huy Chiểu', 'khauhuychieu888@gmail.com', '077 968 1703', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', '3700  COUNTY ROAD 62 GRANT CO 80448-1000 USA
+', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Liên', '1', 'Lục Thúy Liên', 'lucthuylien389@naver.com', '076 458 0176', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Thanh Tùng, Huyện Đầm Dơi, Cà Mau', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Mẫn', '1', 'Huỳnh Thanh Mẫn', 'huynhthanhman626@icloud.com', '090 940 3217', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Hóa Sơn, Huyện Minh Hóa, Quảng Bình', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Uy', '1', 'Ưng Gia Uy', 'unggiauy596@hotmail.com', '099 371 8654', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Mường Sại, Huyện Quỳnh Nhai, Sơn La', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Hà', '1', 'Sái Quang Hà', 'saiquangha2@yahoo.com', '084 129 3056', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Tiến Thắng, Huyện Lý Nhân, Hà Nam
+', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Ngọc', '1', 'Viêm Vân Ngọc', 'viemvanngoc668@naver.com', '035 820 1649', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Nghĩa Đồng, Huyện Nghĩa Hưng, Nam Định', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Tường', '1', 'Khai Thế Tường', 'khaithetuong589@yahoo.com', '078 843 2915', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Phường Quang Trung, Thành phố Thái Bình, Thái Bình', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Thi', '1', 'Phú Uyên Thi', 'phuuyenthi595@yahoo.com', '038 105 3284', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Hòa Điền, Huyện Kiên Lương, Kiên Giang
+', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Uyển', '1', 'Nông Nguyệt Uyển', 'nongnguyetuyen751@naver.com', '099 967 8351', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Vĩnh Thạnh Trung, Huyện Châu Phú, An Giang', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Quảng', '1', 'Lý Ðình Quảng', 'lyinhquang37@google.com', '086 718 9563', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Chu Hương, Huyện Ba Bể, Bắc cạn', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Huy', '1', 'Liên Nhật Huy', 'liennhathuy613@facebook.com', '032 248 0961', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Hữu Lợi, Huyện Yên Thủy, Hoà Bình', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Hoa', '1', 'Khuất Tuyết Hoa', 'khuattuyethoa839@google.com', '078 694 7315', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Phường Cầu Kho, Quận 1, Hồ Chí Minh (tphcm)', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Siêu', '1', 'Phú Ðức Siêu', 'phuucsieu960@icloud.com', '036 104 5932', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Ea Na, Huyện Krông A Na, Đắc Lắc', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Hào', '1', 'Ca Công Hào', 'caconghao52@microsoft.com', '056 063 2879', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', '300 COUNTY ROAD 62 GRANT CO 80448-5013 USA', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Như', '1', 'Thang Tịnh Như', 'thangtinhnhu268@microsoft.com', '034 032 4175', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Tân Trung, Thị xã Gò Công, Tiền Giang', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Khanh', '1', 'Diệp Mai Khanh', 'diepmaikhanh112@google.com', '082 472 5930', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Thị trấn Chùa Hang, Huyện Đồng Hỷ, Thái Nguyên', '0');
+
+INSERT INTO `gymnasium`.`user` (`USERNAME`, `PASSWORD`, `FULLNAME`, `EMAIL`, `PHONENUM`, `IMAGE`, `ADDRESS`, `PERMISSION`) VALUES ('Cường', '1', 'Quán Hùng Cường', 'quanhungcuong325@facebook.com', '084 640 3751', 'https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg', 'Xã Đắk Cấm, Thành phố Kon Tum, Kon Tum
+', '0');
